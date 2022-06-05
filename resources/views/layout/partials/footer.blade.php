ad4e9c86b29b8eefddc893106a12ee57e8c52a5d
Miroslav Holý <mpuco1@azet.sk>
2022-06-05 23:19:23 +0200

    (resources/*) Vue integration. (resources/views) sample layout implementation.

---
+++ resources/views/layout/partials/footer.blade.php
@@ -0,0 +1,3 @@
+<footer class="row">
+    <p class="col-xl-12 col-lg-12 col-md-12 col-sm-12 text-center my-auto" id="copyright">Copyright © 2022</p>
+</footer>
---
+++ resources/views/layout/partials/header.blade.php
@@ -0,0 +1,7 @@
+<header class="row">
+    <div class="col-xl-3 col-lg-3 col-md-12 col-sm-12 text-sm-center text-md-center">
+        <a class="" href="/">
+            <img src="https://www.pngitem.com/pimgs/m/386-3860835_turkey-turkey-country-icon-png-transparent-png.png" alt="Logo countries">
+        </a>
+    </div>
+</header>
